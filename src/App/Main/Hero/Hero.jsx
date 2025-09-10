1586e7739106bfd7bf0b3176b0f330076bb24e92
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-10 21:26:49 +0300

    init

--- src/App/Main/Hero/Hero.jsx
+++ src/App/Main/Hero/Hero.jsx
@@ -1,8 +1,11 @@
+import React, {useEffect, useRef} from 'react';
+import * as THREE from 'three'; // Import Three.js if your effect uses it
+import FOG from 'vanta/src/vanta.fog.js'; // Import the specific Vanta effect
 import './style.scss'
 import Button from "../../components/Button/Button.jsx";
-import { TypeAnimation } from 'react-type-animation';
+import {TypeAnimation} from 'react-type-animation';
 
-const ExampleComponent = () => {
+const TypeComponent = () => {
     return (
         <TypeAnimation
             sequence={[
@@ -18,24 +21,54 @@ const ExampleComponent = () => {
             ]}
             wrapper="span"
             speed={50}
-            style={{ display: 'inline-block' }}
+            style={{display: 'inline-block'}}
             repeat={Infinity}
         />
     );
 };
+
+const VantaBackground = () => {
+    useEffect(() => {
+        FOG({
+            el: '#vanta',
+            THREE: THREE,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            highlightColor: 0x309168,
+            midtoneColor: 0x3c268c,
+            lowlightColor: 0x847ec8,
+            baseColor: 0x161A31,
+            blurFactor: 0.43,
+            speed: 1.20,
+            zoom: 0.40,
+            width: '100%',
+            height: '100%'
+        })
+    }, []);
+
+    return <div className="hero__bg" id='vanta'></div>;
+};
+
 export default function Hero() {
     return <section id='hero' className='hero container'>
-        <hgroup>
+        <VantaBackground/>
+
+        <div className='hero__content container'>
+            <hgroup>
+                <p>
+                    Dynamic Web Magic with Python and JavaScript
+                </p>
+                <h1>
+                    Transforming Concepts into Seamless <span className='pink'>User Experiences</span>
+                </h1>
+            </hgroup>
             <p>
-                Dynamic Web Magic with Python, JavaScript
+                Hi! I’m Oleksii Naidiuk, a <TypeComponent/> Developer based in Ukraine.
             </p>
-            <h1>
-                Transforming Concepts into Seamless <span className='pink'>User Experiences</span>
-            </h1>
-        </hgroup>
-        <p>
-            Hi! I’m Oleksii Naidiuk, a <ExampleComponent/> Developer based in Ukraine.
-        </p>
-        <Button text='See my work'/>
+            <Button text='See my work'/>
+        </div>
     </section>
 }
