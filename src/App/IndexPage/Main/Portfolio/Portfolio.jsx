6af839ab506fa350898c38204f22ea04e73d200e
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-17 14:36:22 +0300

    Portfolio styling

--- src/App/IndexPage/Main/Portfolio/Portfolio.jsx
+++ src/App/IndexPage/Main/Portfolio/Portfolio.jsx
@@ -1,30 +1,28 @@
-import image from '/image.png?url'
+import earth from './images/earth.png?url'
 import './style.scss'
 
 export default function Portfolio() {
     return (
         <div className='portfolio container'>
             <div className='portfolio__grid'>
-                <section className="grid__item-bigger">
+                <section className="grid__item bigger">
                     <div className='img-wrap'>
-                        <img src={image} alt="Portifolio image"/>
+                        <img src={earth} alt="Portifolio image"/>
                     </div>
                     <div className='item__content'>
-
                         <h3>I prioritize client collaboration, fostering open communication </h3>
-
                     </div>
                 </section>
-                <section className="grid__item">
-                    <div className='item__content'>
-                        <h3>I’m very flexible with time zone communications</h3>
-                    </div>
-                </section>
-                <section className="grid__item">
-                    <div className='item__content'>
-                        <h3>My tech stack</h3>
-                    </div>
-                </section>
+                {/*<section className="grid__item">*/}
+                {/*    <div className='item__content'>*/}
+                {/*        <h3>I’m very flexible with time zone communications</h3>*/}
+                {/*    </div>*/}
+                {/*</section>*/}
+                {/*<section className="grid__item">*/}
+                {/*    <div className='item__content'>*/}
+                {/*        <h3>My tech stack</h3>*/}
+                {/*    </div>*/}
+                {/*</section>*/}
             </div>
             <div className="porfolio__grid">
                 <section className="grid__item">
