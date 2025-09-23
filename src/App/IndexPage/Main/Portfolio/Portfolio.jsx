87794f0f61a936ea36232b628f0ca1764f4defc3
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-23 13:04:20 +0300

    рефактор портфолию файл структуры

--- src/App/IndexPage/Main/Portfolio/Portfolio.jsx
+++ src/App/IndexPage/Main/Portfolio/Portfolio.jsx
@@ -1,82 +1,12 @@
-import earth_image from './images/earth.png?url'
-import map_image from './images/map.png?url'
-import code_image from './images/code.png?url'
-import skeleton_image from './images/skeleton.png?url'
 import './style.scss'
-import Slider from "./Slider/MySlider.jsx";
+import FirstPart from "@/App/IndexPage/Main/Portfolio/FirstPart/FirstPart.jsx";
+import SecondPart from "@/App/IndexPage/Main/Portfolio/SecondPart/SecondPart.jsx";
 
 export default function Portfolio() {
     return (
         <div id='portfolio' className='portfolio container'>
-            <div className='portfolio__grid'>
-                <section className="grid__item first">
-                    <div className='img-wrap'>
-                        <img src={earth_image} alt="Portifolio image"/>
-                    </div>
-                    <div className='item__content'>
-                        <h3>I prioritize client collaboration, fostering open communication </h3>
-                    </div>
-                </section>
-                <section className="grid__item second">
-                    <div className='img-wrap fill'>
-                        <img src={map_image} alt="Map image"/>
-                    </div>
-
-                    <div className='item__content inverted light-bg'>
-                        <div className="floating">
-                            <span>USA</span>
-                            <span>Europe</span>
-                            <span>Ukraine</span>
-                        </div>
-                        <h3>I’m very flexible with time zone communications</h3>
-                    </div>
-                </section>
-                <section className="grid__item third">
-                    <div className='item__content with-slider'>
-                        <hgroup>
-                            <p>I constantly try to improve</p>
-                            <h3>My tech stack</h3>
-                        </hgroup>
-                        <Slider/>
-                    </div>
-                </section>
-            </div>
-            <div className="portfolio__grid less-higher">
-                <section className="grid__item div1">
-                    <div className='img-wrap down-scaled right-left-corner'>
-                        <img src={skeleton_image} alt="Code image"/>
-                    </div>
-                    <div className='item__content inverted'>
-                        <h4 >
-                            Tech enthusiast with a passion for development.
-                        </h4>
-                    </div>
-                </section>
-                <section className="grid__item inverted div2">
-                    <div className='item__content '>
-                        <h4 className={'centered'}>
-                            Do you want to start a project together?
-                        </h4>
-                        <a href="/contact" className={'btn'}>
-                            Contact me
-                        </a>
-
-                    </div>
-                </section>
-                <section className="grid__item div3">
-                    <div className='img-wrap down-scaled'>
-                        <img src={code_image} alt="Code image"/>
-                    </div>
-                    <div className='item__content center'>
-                        <hgroup>
-                            <p className={'bigger'}>The Inside Scoop</p>
-                            <h4>
-                                Currently building a JS Animation library
-                            </h4>
-                        </hgroup>
-                    </div>
-                </section>
-            </div>
+            <FirstPart/>
+            <SecondPart/>
         </div>
     )
 }
