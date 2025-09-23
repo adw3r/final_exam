7c17296688669df536581284d0e0c8b59dbc9e63
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-23 13:48:01 +0300

    rafactor

--- src/App/IndexPage/Main/Portfolio/Portfolio.jsx
+++ src/App/IndexPage/Main/Portfolio/Portfolio.jsx
@@ -1,100 +1,14 @@
 import './style.scss'
-import earth_image from './images/earth.png?url'
-import map_image from './images/map.png?url'
-import code_image from './images/code.png?url'
-import skeleton_image from './images/skeleton.png?url'
-import Slider from "./Slider/MySlider.jsx";
-import Button from "@/App/components/Button/Button.jsx";
-import React, {useEffect} from "react";
-import FOG from "vanta/src/vanta.fog.js";
-import * as THREE from "three";
-
-function Vanta({selector, id}) {
-    useEffect(() => {
-        FOG({
-            el: selector,
-            THREE: THREE,
-            highlightColor: 0x309168,
-            midtoneColor: 0x3c268c,
-            lowlightColor: 0x847ec8,
-            baseColor: 0x161A31,
-            blurFactor: 1,
-            speed: 4, zoom: .2})
-    }, [selector]);
-    return <div id={id}></div>;
-}
+import React from "react";
+import FirstPart from "@/App/IndexPage/Main/Portfolio/FirstPart/FirstPart.jsx";
+import SecondPart from "@/App/IndexPage/Main/Portfolio/SecondPart/SecondPart.jsx";
 
 export default function Portfolio() {
 
     return (
         <div id='portfolio' className='portfolio container'>
-            <div className='portfolio__grid'>
-                <section className="grid__item first">
-                    <div className='img-wrap'>
-                        <img src={earth_image} alt="Portifolio image"/>
-                    </div>
-                    <div className='item__content'>
-                        <h3>I prioritize client collaboration, fostering open communication </h3>
-                    </div>
-                </section>
-                <section className="grid__item second">
-                    <div className='img-wrap fill'>
-                        <img src={map_image} alt="Map image"/>
-                    </div>
-
-                    <div className='item__content inverted light-bg'>
-                        <div className="floating">
-                            <span>USA</span>
-                            <span>Europe</span>
-                            <span>Ukraine</span>
-                        </div>
-                        <h3>I’m very flexible with time zone communications</h3>
-                    </div>
-                </section>
-                <section className="grid__item third">
-                    <div className='item__content with-slider'>
-                        <hgroup>
-                            <p>I constantly try to improve</p>
-                            <h3>My tech stack</h3>
-                        </hgroup>
-                        <Slider/>
-                    </div>
-                </section>
-            </div>
-            <div className="portfolio__grid less-higher">
-                <section className="grid__item div1">
-                    <div className='img-wrap down-scaled right-left-corner'>
-                        <img src={skeleton_image} alt="Code image"/>
-                    </div>
-                    <div className='item__content inverted'>
-                        <h4>
-                            Tech enthusiast with a passion for development.
-                        </h4>
-                    </div>
-                </section>
-                <section className="grid__item inverted div2">
-                    <Vanta id='profile_vanta' selector='#profile_vanta' />
-                    <div className='item__content no-bg'>
-                        <h4 className={'centered'}>
-                            Do you want to start a project together?
-                        </h4>
-                        <Button text={'Contact me'} className={'small center'}/>
-                    </div>
-                </section>
-                <section className="grid__item div3">
-                    <div className='img-wrap down-scaled'>
-                        <img src={code_image} alt="Code image"/>
-                    </div>
-                    <div className='item__content center'>
-                        <hgroup>
-                            <p className={'bigger'}>The Inside Scoop</p>
-                            <h4>
-                                Currently building a JS Animation library
-                            </h4>
-                        </hgroup>
-                    </div>
-                </section>
-            </div>
+            <FirstPart/>
+            <SecondPart/>
         </div>
     )
 }
---
+++ src/App/IndexPage/Main/Portfolio/SecondPart/SecondPart.jsx
@@ -0,0 +1,65 @@
+import skeleton_image from "@/App/IndexPage/Main/Portfolio/SecondPart/skeleton.png";
+import Button from "@/App/components/Button/Button.jsx";
+import code_image from "@/App/IndexPage/Main/Portfolio/SecondPart/code.png";
+import React, {useEffect} from "react";
+import FOG from "vanta/src/vanta.fog.js";
+import * as THREE from "three";
+
+
+
+function Vanta({selector, id}) {
+    useEffect(() => {
+        FOG({
+            el: selector,
+            THREE: THREE,
+            highlightColor: 0x309168,
+            midtoneColor: 0x3c268c,
+            lowlightColor: 0x847ec8,
+            baseColor: 0x161A31,
+            blurFactor: 1,
+            speed: 4, zoom: .2
+        })
+    }, [selector]);
+    return <div id={id}></div>;
+}
+
+
+
+const SecondPart = () => {
+    return <div className="portfolio__grid less-higher">
+        <section className="grid__item div1">
+            <div className='img-wrap down-scaled right-left-corner'>
+                <img src={skeleton_image} alt="Code image"/>
+            </div>
+            <div className='item__content inverted'>
+                <h4>
+                    Tech enthusiast with a passion for development.
+                </h4>
+            </div>
+        </section>
+        <section className="grid__item inverted div2">
+            <Vanta id='profile_vanta' selector='#profile_vanta'/>
+            <div className='item__content no-bg'>
+                <h4 className={'centered'}>
+                    Do you want to start a project together?
+                </h4>
+                <Button text={'Contact me'} className={'small center'}/>
+            </div>
+        </section>
+        <section className="grid__item div3">
+            <div className='img-wrap down-scaled'>
+                <img src={code_image} alt="Code image"/>
+            </div>
+            <div className='item__content center'>
+                <hgroup>
+                    <p className={'bigger'}>The Inside Scoop</p>
+                    <h4>
+                        Currently building a JS Animation library
+                    </h4>
+                </hgroup>
+            </div>
+        </section>
+    </div>
+
+}
+export default SecondPart;
