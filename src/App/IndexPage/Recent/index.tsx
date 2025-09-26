6378dc82783ecc67e800b6e58eb24e8e8c98aca9
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-26 15:24:03 +0300

    recent section

--- src/App/IndexPage/Recent/index.tsx
+++ src/App/IndexPage/Recent/index.tsx
@@ -1,40 +1,40 @@
 import Card from "./Card";
-import card_image_1 from '/1736b40192687e0826ec81f7913b4c9f3f82d131.png'
-import card_image_2 from '/9cc194a5253deb206776ff54a02b41c67b9d6954.png'
-import card_image_3 from '/9ce5ed49ef17e55c59100d5ee26d94c7bd0d0172.png'
-import card_image_4 from '/c3d3ceeac6eedd977a56a42e63a70cf89162be38.png'
+import mono_automation_image from '/img.png'
+import proxy_parser_image from '/img_1.png'
+import rpi_sms_image from '/img_3.png'
+import copart_image from '/img_2.png'
 
 import './style.scss'
 
 export default function Recent() {
     const items = [
         {
-            title: '3D Solar System Planets to Explore',
-            body: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
-            url: 'https://adw3r.github.io/jsExam/',
+            title: 'Personal budget app',
+            body: 'Integration of MonoBank API and Google`s spreadsheets in one app using FastAPI + HTMX stack',
+            url: 'https://adw3r.tech/',
             icons: [],
-            img: card_image_1
+            img: mono_automation_image
         },
         {
-            title: 'Yoom - Video Conferencing App',
-            body: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
-            url: 'https://adw3r.github.io/jw_hw_33/',
+            title: 'Free Proxy scraper',
+            body: 'A high-performance, asynchronous proxy scraper and validator that discovers free proxies from GitHub repositories and validates their functionality.',
+            url: 'https://github.com/adw3r/proxy_parser',
             icons: [],
-            img: card_image_2
+            img: proxy_parser_image
         },
         {
-            title: 'AI Image SaaS - Canva Application',
-            body: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
-            url: 'https://adw3r.github.io/jw_hw_33/',
+            title: 'Low-budget sms receiver',
+            body: 'Raspberry PI with SMS receiver for robotic automation scripts. 1 phone card - No headaches',
+            url: 'https://github.com/adw3r/rpi_sms_receiver',
             icons: [],
-            img: card_image_3
+            img: rpi_sms_image
         },
         {
-            title: 'AI Image SaaS - Canva Application',
-            body: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
-            url: 'https://adw3r.github.io/jw_hw_33/',
+            title: 'Copart Scraper',
+            body: 'An automative online auction scraper with one-line bootup',
+            url: 'https://github.com/adw3r/copart',
             icons: [],
-            img: card_image_4
+            img: copart_image
         },
     ]
 
