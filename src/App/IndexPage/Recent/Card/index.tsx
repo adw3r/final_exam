628e9ced72223a80ce03fc8025bc50a528d4d300
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-09-26 15:03:15 +0300

    recent section

--- src/App/IndexPage/Recent/Card/index.tsx
+++ src/App/IndexPage/Recent/Card/index.tsx
@@ -1,26 +1,42 @@
 import {ReactElement} from "react";
 import './style.scss'
 
-type CardProps = { title: string, body: string, url: string, img: string, icons: string[] }
+import card_bg from './card_bg.png'
 
-const Card = ({title, body, url, img, icons}: CardProps): ReactElement => <div className="card ">
-    <div className="img-wrap">
-        <img src={img} alt="Card image" />
+interface CardProps {
+    title: string,
+    body: string,
+    url: string,
+    img: string,
+    icons: string[]
+}
+
+
+const Card = ({title, body, url, img, icons}: CardProps): ReactElement => <article className="card">
+    <div className="card__img" style={{backgroundImage: `url(${card_bg})`}}>
+        <div className="img-wrap">
+            <img src={img}/>
+        </div>
     </div>
-    <span className="title">
-        {title}
-    </span>
-    <p className={'body'}>
-        {body}
-    </p>
-
-    <ul>
-        {icons.length > 0 && icons.map((icon: string) => (
-            <li key={icon} className={'icon'}>
-                {icon}
-            </li>
-        ))}
-    </ul>
-    <a href={url}>Check out</a>
-</div>;
+    <div className={'card__content'}>
+        <div className={'content__title'}>
+            <h3 className="title">
+                {title}
+            </h3>
+            <p className={'body'}>
+                {body}
+            </p>
+        </div>
+        <div className="content__items">
+            <ul>
+                {icons.length > 0 && icons.map((icon: string) => (
+                    <li key={icon} className={'icon'}>
+                        {icon}
+                    </li>
+                ))}
+            </ul>
+            <a href={url}>Check out</a>
+        </div>
+    </div>
+</article>;
 export default Card;
--- src/App/IndexPage/Recent/index.tsx
+++ src/App/IndexPage/Recent/index.tsx
@@ -1,12 +1,41 @@
 import Card from "./Card";
-import card_1_image from '/Card.png'
-import card_2_image from '/Card (1).png'
+import card_image_1 from '/1736b40192687e0826ec81f7913b4c9f3f82d131.png'
+import card_image_2 from '/9cc194a5253deb206776ff54a02b41c67b9d6954.png'
+import card_image_3 from '/9ce5ed49ef17e55c59100d5ee26d94c7bd0d0172.png'
+import card_image_4 from '/c3d3ceeac6eedd977a56a42e63a70cf89162be38.png'
+
 import './style.scss'
 
 export default function Recent() {
     const items = [
-        {title: 'Monticello BeetRoot homework', body: 'Body', url: 'https://adw3r.github.io/jsExam/', icons: [], img: card_1_image},
-        {title: 'OMDB API Front-end', body: 'Body', url: 'https://adw3r.github.io/jw_hw_33/', icons: [], img: card_2_image},
+        {
+            title: '3D Solar System Planets to Explore',
+            body: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+            url: 'https://adw3r.github.io/jsExam/',
+            icons: [],
+            img: card_image_1
+        },
+        {
+            title: 'Yoom - Video Conferencing App',
+            body: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
+            url: 'https://adw3r.github.io/jw_hw_33/',
+            icons: [],
+            img: card_image_2
+        },
+        {
+            title: 'AI Image SaaS - Canva Application',
+            body: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+            url: 'https://adw3r.github.io/jw_hw_33/',
+            icons: [],
+            img: card_image_3
+        },
+        {
+            title: 'AI Image SaaS - Canva Application',
+            body: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+            url: 'https://adw3r.github.io/jw_hw_33/',
+            icons: [],
+            img: card_image_4
+        },
     ]
 
     return <section className={'recent container'}>
